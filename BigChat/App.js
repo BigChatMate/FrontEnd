5bdadbef0b0442cc117c161b5c224d51a3c35ac0
HarminderK <harminderk@hotmail.ca>
2018-10-27 20:01:30 -0700

    App.js codacy fix

--- BigChat/App.js
+++ BigChat/App.js
@@ -50,7 +50,7 @@ export default class App extends React.Component {
 });
 
 ChatNav.navigationOptions=({navigation})=>{
-  if(navigation.state.index!=0){
+  if(navigation.state.index !== 0){
     return{
       tabBarVisible : false,
     };
